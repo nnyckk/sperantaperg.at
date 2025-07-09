df24bcf1231daca8012d2342c4278977be3cd363
nnyckk <neagoe.nick@gmail.com>
2025-07-09 10:35:18 +0200

    Fixed JS on smartphone view

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,28 @@
 
 // Navigation Wrapper background colored on scroll;
-window.addEventListener("scroll", function () {
-    const navbar = document.getElementById("navigationWrapper");
-    if (window.scrollY > 30) {
-      navbar.classList.add("scrolled");
+const navbar = document.getElementById("navigationWrapper");
+  const mediaQuery = window.matchMedia("(min-width: 980px)");
+
+  function handleScroll() {
+    if (mediaQuery.matches) {
+      if (window.scrollY > 30) {
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
     } else {
+      // On small screens: always remove the class
       navbar.classList.remove("scrolled");
     }
-  });
+  }
+
+  window.addEventListener("scroll", handleScroll);
+  mediaQuery.addEventListener("change", handleScroll); // re-check if resized
 
 
 
 
-
-
-
+// Navigation Burger Menu;
 
 var navLinks = document.getElementById("navLinks");
 
