01b62da8974099a8cbfa420af3f18c854e2e035b
nnyckk <neagoe.nick@gmail.com>
2025-07-09 11:51:40 +0200

    Changes in phone menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,21 @@
 
 // Navigation Wrapper background colored on scroll;
 const navbar = document.getElementById("navigationWrapper");
-  const mediaQuery = window.matchMedia("(min-width: 980px)");
-
+  
   function handleScroll() {
-    if (mediaQuery.matches) {
       if (window.scrollY > 30) {
         navbar.classList.add("scrolled");
       } else {
         navbar.classList.remove("scrolled");
       }
-    } else {
-      // On small screens: always remove the class
-      navbar.classList.remove("scrolled");
-    }
   }
-
   window.addEventListener("scroll", handleScroll);
-  mediaQuery.addEventListener("change", handleScroll); // re-check if resized
 
 
 
 
 // Navigation Burger Menu;
-var navLinks = document.getElementById("navLinks");
+const navLinks = document.getElementById("navLinks");
 
 function showMenu(){
     navLinks.style.opacity = "1";
