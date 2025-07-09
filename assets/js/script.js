90cc71da86416bc7d129069ed84737a16bd1c9e1
nnyckk <neagoe.nick@gmail.com>
2025-07-09 10:02:16 +0200

    Manny Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+
+// Navigation Wrapper background colored on scroll;
+window.addEventListener("scroll", function () {
+    const navbar = document.getElementById("navigationWrapper");
+    if (window.scrollY > 30) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+
+
+
+
+
+
+
+
 var navLinks = document.getElementById("navLinks");
 
 function showMenu(){
@@ -8,3 +26,5 @@ function hideMenu(){
     navLinks.style.opacity = "0";
     navLinks.style.pointerEvents = "none"
 }
+
+
