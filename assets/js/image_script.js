63d6d95e4e63b604f88e440f10988c47a08fa981
nnyckk <neagoe.nick@gmail.com>
2025-07-17 11:11:41 +0200

    English classes

--- assets/js/image_script.js
+++ assets/js/image_script.js
@@ -1,20 +1,20 @@
 
-// IMAGE FADE IN EFFECT
-function fadeInPeScroll() {
+// Image fade in effect
+function fadeInOnScroll() {
     $('.image-cell-fadeIn').each(function(index) {
       const $this = $(this);
       const topImg = $this.offset().top;
       const bottomWindow = $(window).scrollTop() + $(window).height();
 
-      if (bottomWindow > topImg + 150 && !$this.hasClass('vizibila')) {
+      if (bottomWindow > topImg + 150 && !$this.hasClass('visible')) {
         setTimeout(() => {
-          $this.addClass('vizibila');
-        }, index * 300); // întârziere între imagini
+          $this.addClass('visible');
+        }, index * 300); // Deley for the images
       }
     });
   }
 
   $(document).ready(function() {
-    $(window).on('scroll', fadeInPeScroll);
-    fadeInPeScroll(); // apel și la încărcare
+    $(window).on('scroll', fadeInOnScroll);
+    fadeInOnScroll();
   });
