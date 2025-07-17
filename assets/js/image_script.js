d0e214e6c6a86d75ded1279e76e69184a88c6607
nnyckk <neagoe.nick@gmail.com>
2025-07-17 11:14:42 +0200

    A

--- assets/js/image_script.js
+++ assets/js/image_script.js
@@ -9,11 +9,10 @@ function fadeInOnScroll() {
       if (bottomWindow > topImg + 150 && !$this.hasClass('visible')) {
         setTimeout(() => {
           $this.addClass('visible');
-        }, index * 300); // Deley for the images
+        }, index * 300); // Delay for the images
       }
     });
   }
-
   $(document).ready(function() {
     $(window).on('scroll', fadeInOnScroll);
     fadeInOnScroll();
