3febe426992e5c33d96a041cc1f5f633a65bd515
nnyckk <neagoe.nick@gmail.com>
2025-07-21 14:52:51 +0200

    Link fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Hamburger Menu Effect;
-    const hamburger = document.getElementById("nav-icon-hamburger");
-    const navMenu = document.getElementById("nav-menu");
-    const navBar = document.getElementById("navigationWrapper");
+  // Hamburger Menu Effect;
+  const hamburger = document.getElementById("nav-icon-hamburger");
+  const navMenu = document.getElementById("nav-menu");
+  const navBar = document.getElementById("navigationWrapper");
 
-    hamburger.addEventListener("click", function () {
-      this.classList.toggle("open");
-      navMenu.classList.toggle("active");
-      navBar.classList.toggle("active");
-    });
+  hamburger.addEventListener("click", function () {
+    this.classList.toggle("open");
+    navMenu.classList.toggle("active");
+    navBar.classList.toggle("active");
+  });
 
-
-    // Navigation Wrapper background colored on scroll;
-    const navbar = document.getElementById("navigationWrapper");
-    function handleScroll() {
+  // Navigation Wrapper background colored on scroll;
+  const navbar = document.getElementById("navigationWrapper");
+  function handleScroll() {
     if (window.scrollY > 30) {
       navbar.classList.add("scrolled");
     } else {
       navbar.classList.remove("scrolled");
     }
-}
-window.addEventListener("scroll", handleScroll);
+  }
+  window.addEventListener("scroll", handleScroll);
 
+  // Active Link On URL Effects;
+  const links = document.querySelectorAll("nav a");
+  const currentPath = window.location.pathname;
 
-// Active Link On URL Effects;
-const links = document.querySelectorAll("nav a");
-const currentPath = window.location.pathname;
-
-  links.forEach(link => {
+  links.forEach((link) => {
     if (link.getAttribute("href") === currentPath) {
       link.classList.add("active");
     }
   });
 
-
-    //NO Scroll when Phone Menu is open;
-  document.getElementById("nav-icon-hamburger").addEventListener("click", () => {
-    document.body.classList.toggle("no-scroll");
-  });
-
-
-
-
-  
-
+  //NO Scroll when Phone Menu is open;
+  document
+    .getElementById("nav-icon-hamburger")
+    .addEventListener("click", () => {
+      document.body.classList.toggle("no-scroll");
+    });
 });
