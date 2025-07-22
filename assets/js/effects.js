5c12f6ba62c240a5f944ffcbd418093e6e7ffbe9
nnyckk <neagoe.nick@gmail.com>
2025-07-22 09:18:01 +0200

    Bug Fixes

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -1,18 +1,20 @@
-// FadeIn effect
-function fadeInOnScroll() {
-    $('.fadeIn').each(function(index) {
+document.addEventListener("DOMContentLoaded", function () {
+  // FadeIn effect
+  function fadeInOnScroll() {
+    $(".fadeIn").each(function (index) {
       const $this = $(this);
       const topImg = $this.offset().top;
       const bottomWindow = $(window).scrollTop() + $(window).height();
 
-      if (bottomWindow > topImg + 100 && !$this.hasClass('visible')) {
+      if (bottomWindow > topImg + 100 && !$this.hasClass("visible")) {
         setTimeout(() => {
-          $this.addClass('visible');
+          $this.addClass("visible");
         }, index * 100); // Delay for the images
       }
     });
   }
-  $(document).ready(function() {
-    $(window).on('scroll', fadeInOnScroll);
+  $(document).ready(function () {
+    $(window).on("scroll", fadeInOnScroll);
     fadeInOnScroll();
   });
+});
