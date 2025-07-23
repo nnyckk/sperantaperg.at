972ae26101476dbfbf83eea3f61765ed9c92bf99
nnyckk <neagoe.nick@gmail.com>
2025-07-23 14:49:41 +0200

    Big UpDate: Pictures Formated and some minor bugs

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const topImg = $this.offset().top;
       const bottomWindow = $(window).scrollTop() + $(window).height();
 
-      if (bottomWindow > topImg + 80 && !$this.hasClass("visible")) {
+      if (bottomWindow > topImg + 100 && !$this.hasClass("visible")) {
         setTimeout(() => {
           $this.addClass("visible");
         }, index * 50); // Delay for the images
