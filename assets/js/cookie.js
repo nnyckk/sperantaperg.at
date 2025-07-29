96e19c0724bafd8d07e3ada93e06bf36b9154b3b
nnyckk <neagoe.nick@gmail.com>
2025-07-29 13:00:05 +0200

    Impressum, Data collection, Cookies Banner

---
+++ assets/js/cookie.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function handleCookieConsent(accepted) {
+    localStorage.setItem("cookieConsent", accepted ? "accepted" : "refused");
+    document.getElementById("cookie-banner").style.display = "none";
+
+    if (accepted) {
+      console.log("Cookies acceptate / Cookies akzeptiert");
+
+      // Google Analytics
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+
+      gtag("config", "G-KE98B3X7E5");
+    } else {
+      console.log("Cookies refuzate / Cookies abgelehnt");
+    }
+  }
+
+  window.addEventListener("load", function () {
+    const consent = localStorage.getItem("cookieConsent");
+    if (!consent) {
+      setTimeout(() => {
+        document.getElementById("cookie-banner").style.display = "block";
+      }, 5000); // delay 5 seconds
+    }
+  });
+});
